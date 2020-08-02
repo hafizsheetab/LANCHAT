48234762c9671c5a244650ed6707b693f632bb57
hafizsheetab <muhtaseen@iut-dhaka.edu>
2020-08-03 05:36:33 +0600

    #3

--- src/SourceCode/Client.java
+++ src/SourceCode/Client.java
@@ -1,4 +1,21 @@
 package SourceCode;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost",5000);
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
